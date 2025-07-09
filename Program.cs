6a6375f4e002bb4365ef5048cd1ba674c2cb1956
Kirimoshi <baronaxaro@gmail.com>
2025-07-09 21:54:39 +0200

    Add Identity feature for user authentication and account management.
    Includes Identity infrastructure with views (`Login`, `Register`, `ForgotPassword`, `ChangePassword`, `ExternalLogin`, and `Manage`) and Razor Pages for managing user accounts. Integrates ASP.NET Core Identity with custom layouts and scripts.

--- Program.cs
+++ Program.cs
@@ -4,30 +4,58 @@ using Microsoft.Extensions.DependencyInjection;
 using ProjektHaushaltsbuch.Data;
 using ProjektHaushaltsbuch.SeedData;
 using ProjektHaushaltsbuch.Web.Mappings;
+using Microsoft.AspNetCore.Identity;
+using ProjektHaushaltsbuch.Data.Identity;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
 builder.Services.AddDbContext<ProjektHaushaltsbuchContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProjektHaushaltsbuchContext") ?? throw new InvalidOperationException("Connection string 'ProjektHaushaltsbuchContext' not found.")));
+builder.Services.AddDbContext<ApplicationIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false; // For production change to true
+            // options.User.RequireUniqueEmail = true;
+        }
+    )
+    .AddEntityFrameworkStores<ApplicationIdentityContext>();
 
 builder.Services.AddSerilog();
 builder.Services.AddAutoMapper(typeof(UserProfile));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        IConfigurationSection googleAuthNSection =
+            config.GetSection("Authentication:Google");
+        options.ClientId = googleAuthNSection["ClientId"];
+        options.ClientSecret = googleAuthNSection["ClientSecret"];
+
+        // options.ClientId = config["Authentication__Google__ClientId"];
+        // options.ClientSecret = config["Authentication__Google__ClientSecret"];
+    });
 
 var app = builder.Build();
 
+// Seeding DB
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
 
     try
     {
-SeedData.Initialize(services);
+        SeedData.Initialize(services);
     }
     catch (Exception e)
     {
@@ -36,9 +64,15 @@ SeedData.Initialize(services);
 }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+    Console.WriteLine("Running in Development mode");
+}
+else
+{
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -46,9 +80,12 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
         name: "default",
