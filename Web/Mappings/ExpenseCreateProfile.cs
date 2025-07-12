99ae18aea068d2d712680ef300c7a3bd13151148
Kirimoshi <baronaxaro@gmail.com>
2025-07-12 17:26:08 +0200

    Implement `ExpenseCreateViewModel` and update `ExpenseController` and `Create` view to use it. Add data annotations, AutoMapper profile, and UI enhancements for improved expense creation experience. Update `ExpenseConfiguration` and introduce localized labels.

---
+++ Web/Mappings/ExpenseCreateProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ProjektHaushaltsbuch.Models;
+using ProjektHaushaltsbuch.Web.ViewModels;
+
+namespace ProjektHaushaltsbuch.Web.Mappings;
+
+public class ExpenseCreateProfile: Profile
+{
+    public ExpenseCreateProfile()
+    {
+        CreateMap<ExpenseCreateViewModel, ExpenseModel>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id == Guid.Empty ? Guid.NewGuid() : src.Id))
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Wird durch DB-Default gesetzt
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Wird durch Business Logic gesetzt
+            .ForMember(dest => dest.Tags,
+                opt => opt.MapFrom(src => src.Tags != null ? string.Join(",", src.Tags) : null))
+            .ForMember(dest => dest.AttachmentUrls,
+                opt => opt.MapFrom(src => src.AttachmentUrls != null ? string.Join(",", src.AttachmentUrls) : null))
+            .ForMember(dest => dest.User, opt => opt.Ignore()) // Navigation Property - wird nicht gemappt
+            .ForMember(dest => dest.Category, opt => opt.Ignore()) // Navigation Property - wird nicht gemappt
+            .ForMember(dest => dest.UserId, opt => opt.Ignore()); // Wird durch Business Logic gesetzt
+    }
+}
